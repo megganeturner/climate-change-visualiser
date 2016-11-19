ecde7fa65739aff5c21cd61ddc30366f9a4c1a1f
megganeturner <meggan.turner@hotmail.com>
2016-11-19 15:11:37 +1100

    controls added, earth rendered

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,11 +4,10 @@ var app = app || {};
 app.init = function(){
 
   app.scene = new THREE.Scene();
-
   app.width = window.innerWidth;
   app.height = window.innerHeight;
 
-  app.camera = new THREE.PerspectiveCamera(60, app.width/app.height, 0.1, 1000);
+  app.camera = new THREE.PerspectiveCamera(75, app.width/app.height, 0.1, 1000);
 
   app.camera.position.x = 0;
   app.camera.position.y = 0;
@@ -18,12 +17,46 @@ app.init = function(){
 
   app.renderer = new THREE.WebGLRenderer();
   app.renderer.setSize(app.width, app.height);
-  app.renderer.setClearColor(0x000000);
+  document.body.appendChild(app.renderer.domElement);
+
+  var earthGeometry = new THREE.SphereGeometry(2, 32, 32);
+  var earthMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    wireframe: true
+  });
+
+  app.earth = new THREE.Mesh(earthGeometry, earthMaterial);
+
+  var light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(0,1,1).normalize();
+
+  app.scene.add(new THREE.AmbientLight(0x333333), app.earth);
+
+
+
+  app.controls = new THREE.TrackballControls(app.camera);
+
   app.animate();
 };
 
 
 app.animate = function(){
+
+  if (app.camera.position.z >=200) {
+    camera.position.z = 200;
+  }
+
+  if (app.camera.position.z < 150) {
+    $('.saganQuote').fadeOut(1000);
+    $('.scrollToZoom').fadeOut(1000);
+  } else {
+    $('.saganQuote').fadeIn(1000);
+    $('.scrollToZoom').fadeIn(1000);
+  }
+
+  app.controls.update();
+
+  app.earth.rotation.y += 0.002;
 
   app.renderer.render(app.scene, app.camera);
 
